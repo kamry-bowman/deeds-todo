38a0f39d94e2f4c9a8bc32bfc092727cc4e96f0a
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-13 18:19:22 -0600

    todos displaying

--- native-client/App.js
+++ native-client/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Amplify, { API } from 'aws-amplify';
 import MainNavigator from './components/MainNavigator';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+// import { createHttpLink } from 'apollo-link-http';
+// import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import {
   userPoolId,
@@ -18,17 +24,52 @@ Amplify.configure({
     region,
     userPoolWebClientId,
   },
-  API: {
-    graphql_endpoint: GQL_ENDPOINT,
-  },
+});
+
+// const httpLink = createHttpLink({ uri: GQL_ENDPOINT });
+
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+const client = new ApolloClient({
+  uri: GQL_ENDPOINT,
+  cache: new InMemoryCache(),
 });
 
 class App extends React.Component {
   componentDidMount() {
     console.log('App mounted');
+    client
+      .query({
+        query: gql`
+          query {
+            todos {
+              title
+              id
+              description
+              completed
+            }
+          }
+        `,
+      })
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
   }
   render() {
-    return <MainNavigator />;
+    return (
+      <ApolloProvider client={client}>
+        <MainNavigator />
+      </ApolloProvider>
+    );
   }
 }
 
--- native-client/views/TodoList.js
+++ native-client/views/TodoList.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import AddTodo from '../components/AddTodo';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 export default class TodoList extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>TodoList</Text>
-        <AddTodo />
-        <Button
-          title="Single Todo"
-          onPress={() => this.props.navigation.navigate('SingleTodo')}
-        />
-      </View>
+      <Query
+        query={gql`
+          query {
+            todos {
+              title
+              id
+              description
+              completed
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => (
+          <View style={styles.container}>
+            <Text>TodoList</Text>
+            <AddTodo />
+            <Button
+              title="Single Todo"
+              onPress={() => this.props.navigation.navigate('SingleTodo')}
+            />
+            {error ? (
+              <Text>error!</Text>
+            ) : loading ? (
+              <Text>loading...</Text>
+            ) : (
+              data.todos.map(todo => (
+                <Text key={todo.id}>
+                  {`${todo.title} - ${todo.description}`}
+                </Text>
+              ))
+            )}
+          </View>
+        )}
+      </Query>
     );
   }
 }
